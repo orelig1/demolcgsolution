6a8056ff509dc26c4927f4ca87c1e86811e28c5f
orelig1 <aurelie.gauthier11@gmail.com>
2024-09-30 09:05:50 -0400

    fix agnico

--- src/components/NosClients.js
+++ src/components/NosClients.js
@@ -7,7 +7,7 @@ import TroisRiviere_logo from '../images/TroisRiviere_logo.png'
 import BRP_logo from '../images/BRP_logo.png'
 import PMI_logo from '../images/PMI_logo.png'
 import logo_Giro1 from '../images/logo_Giro1.png'
-import agnico from '../images/Agnico.png'
+import agnico from '../images/agnico.png'
 import AIM_logo from '../images/AIM_logo.jpg'
 import AIMQ_logo from '../images/AIMQ_logo.png'
 import { Box } from '@mui/material';
@@ -55,7 +55,7 @@ const itemData = [
   },
   {
     img: agnico,
-    title: 'AGnico Eagle',
+    title: 'Agnico Eagle',
   },
   {
     img: 'https://www.jobillico.com/medias/logo-entreprise/0/0/exp_logo_2254_fr_2020_05_04_13_59_09.jpg',
